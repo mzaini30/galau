74369299f274e1f0c44507b27082b62e47e673d0
mzaini30 <muhzaini30@gmail.com>
2022-06-22 19:25:47 +0800

    feat: tambahkan link review aplikasi

--- app/src/main/assets/docs/.vitepress/config.ts
+++ app/src/main/assets/docs/.vitepress/config.ts
@@ -1,4 +1,6 @@
-export default {
+import {defineConfig} from "vitepress"
+
+export default defineConfig({
   title: "Anti Galau",
   lang: "id",
   base: "/assets/docs/.vitepress/dist/",
@@ -9,6 +11,9 @@ export default {
         "https://github.com/mzaini30/galau/tree/master/app/src/main/assets/docs/:path",
       text: "Edit halaman ini",
     },
+    nav: [
+    	{text: "Review aplikasi", link: "https://play.google.com/store/apps/details?id=com.mzaini30.galau"}
+    ],
     socialLinks: [
       { icon: "github", link: "https://github.com/mzaini30/galau" },
     ],
@@ -16,10 +21,10 @@ export default {
       {
         items: [
         	{text: "Permulaan", link: "/index.html"},
-          { text: "Penyebab Galau", link: "/penyebab" },
-          { text: "Padahal Udah Kerja", link: "/udah-kerja" },
+          { text: "Penyebab Galau", link: "/penyebab.html" },
+          { text: "Padahal Udah Kerja", link: "/udah-kerja.html" },
         ],
       },
     ],
   },
-};
+})
--- app/src/main/assets/docs/udah-kerja.md
+++ app/src/main/assets/docs/udah-kerja.md
@@ -6,3 +6,19 @@ Apa sih yang membuatmu masih galau? Udah ada kerjaan loh itu. Apa yang masih kur
 - Penghasilan yang masih kurang
 - Bosan
 - Nggak ada ide mau melakukan apa
+
+Mungkin kita akan bahas satu per satu ya...
+
+## Tentang Jodoh yang Tak Kunjung Datang
+
+Permasalahan jodoh ini memang suatu episode dalam kehidupan manusia yang paling galau. Entah itu pasangan yang diharapkan tapi nggak datang, usia yang terus bertambah, teman jomblo yang makin berkurang.
+
+Lantas, terkadang dalam diri ini muncul suatu pertanyaan: Apakah diriku nggak lamtas mendapatkan pasangan yang baik ya?
+
+Lalu, bagaimana cara mengatasinya? Apakah berdiam diri saja cukup?
+
+## Penghasilan yang Masih Kurang
+
+## Bosan
+
+## Nggak Ada Ide Melakukan Apa
