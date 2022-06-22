f5a81d122b7176c5c521514401d65ed6dd939356
Muhammad Zaini <muhzaini30@gmail.com>
2022-06-22 14:19:46 +0800

    Initial commit

---
+++ app/src/main/assets/src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="p-3">
+		<h1 class="text-2xl mb-3">Assalamualaikum</h1>
+		<p class="mb-1">Welcome to this Vue template, cooked by Zen.</p>
+		<p>Go to <router-link class='underline decoration-wavy' to="/about">about page</router-link></p>
+	</div>
+</template>
+
+<style scoped="">
+	h1 {
+		@apply text-center
+	}
+</style>
