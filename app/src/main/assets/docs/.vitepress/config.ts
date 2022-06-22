c4db0f9e2f5066d24375fd59f1ba4d7e4964b820
mzaini30 <muhzaini30@gmail.com>
2022-06-22 16:31:46 +0800

    chore: tambah halaman beranda

--- app/src/main/assets/docs/.vitepress/config.ts
+++ app/src/main/assets/docs/.vitepress/config.ts
@@ -4,12 +4,18 @@ export default {
   base: "/assets/docs/.vitepress/dist/",
   lastUpdated: true,
   themeConfig: {
+    editLink: {
+      pattern:
+        "https://github.com/mzaini30/galau/tree/master/app/src/main/assets/docs/:path",
+      text: "Edit halaman ini",
+    },
     socialLinks: [
       { icon: "github", link: "https://github.com/mzaini30/galau" },
     ],
     sidebar: [
       {
         items: [
+        	{text: "Permulaan", link: "/index.html"},
           { text: "Penyebab Galau", link: "/penyebab" },
           { text: "Padahal Udah Kerja", link: "/udah-kerja" },
         ],
